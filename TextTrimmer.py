9289462844e831df6912158c37dec0e39fe1df81
Kevin George <17203241+kgeok@users.noreply.github.com>
2021-08-12 17:12:51 -0400

    Add files via upload

--- TextTrimmer.py
+++ TextTrimmer.py
@@ -1,41 +1,54 @@
-#Written by Kevin George
-#Version 0.1
 import os
 import sys
 
-print (sys.argv[1])
-inputlines = []
-buffer = []
-Beginning = 0
-End = 0
-bvalue = sys.argv[2]
-evalue = sys.argv[3]
-inputfile = open(sys.argv[1], "r")
-outputnamebuffer = str(sys.argv[1])
-outputfile = open(outputnamebuffer.replace('.txt', '_EDITED.txt'), "w+")
+version = "0.2"
 
-for x in inputfile:
-    inputlines.append(x)
+if (sys.argv[1] == "-h"):
+    print ("")
+    print ("-------------Text Trimmer " + version + "-------------")
+    print ("-------------Written by Kevin George-------------")
+    print ("-h for usage and help")
+    print ("-t to trim text file")
+    print ("Usage: ./TextTrimmer -t [FileName.txt] [Beginning Text] [End Text]")
+    print ("")
 
-for i in range(len(inputlines)):
-    if inputlines[i] == bvalue:
-        #print("Found Beginning")
-        Beginning = i
-        #print(i)
-        
-for i in range(len(inputlines)):
-    if inputlines[i] == evalue:
-        #print("Found End")
-        End = i
-        #print(i)
+elif (sys.argv[1] == "-t"):
+    print (sys.argv[2])
+    inputlines = []
+    buffer = []
+    Beginning = 0
+    End = 0
+    bvalue = sys.argv[3]
+    evalue = sys.argv[4]
+    inputfile = open(sys.argv[2], "r")
+    outputnamebuffer = str(sys.argv[2])
+    outputfile = open(outputnamebuffer.replace('.txt', '_EDITED.txt'), "w+")
 
-buffer = list(inputlines[Beginning:End])
-#print(buffer)
+    for x in inputfile:
+        inputlines.append(x)
 
-for i in range(len(buffer)):
-    outputfile.write(buffer[i])
+    for i in range(len(inputlines)):
+        if inputlines[i] == bvalue:
+            #print("Found Beginning")
+            Beginning = i
+            #print(i)
+            
+    for i in range(len(inputlines)):
+        if inputlines[i] == evalue:
+            #print("Found End")
+            End = i
+            #print(i)
 
-inputfile.close()
-outputfile.close()
+    buffer = list(inputlines[Beginning:End])
+    #print(buffer)
 
-print ("Done with: " + outputnamebuffer)
+    for i in range(len(buffer)):
+        outputfile.write(buffer[i])
+
+    inputfile.close()
+    outputfile.close()
+
+    print ("Done with: " + outputnamebuffer)
+
+else:
+    print ("Invalid argument, please type -h for a list of all arguments")
